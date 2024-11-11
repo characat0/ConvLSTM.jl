81a8bff09dfb11bef1367723fb9f4f25bb6e59da
Marco Vela <36453977+characat0@users.noreply.github.com>
2024-11-11 16:27:09 -0500

    test for teaching

--- test/convlstm_tests.jl
+++ test/convlstm_tests.jl
@@ -13,4 +13,13 @@
         @test_gradients(__f, x, ps; atol=1.0f-3, rtol=1.0f-3, skip_backends=[AutoReverseDiff(), AutoEnzyme(), AutoTracker()])
     end
 
+    @testset "Conditional - Teaching" begin
+        model = SequenceToSequenceConvLSTM((3, 3), (3, 3), 1, (2, 4), 3, :conditional)
+        ps, st = Lux.setup(Lux.replicate(rng), model)
+        st_test = Lux.testmode(st)
+        y_teach, _ = model(x, ps, st)
+        y_not_teach, _ = model(x, ps, st_test)
+        @assert !(y_teach ≈ y_not_teach)
+    end
+
 end
